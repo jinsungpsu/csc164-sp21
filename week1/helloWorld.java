d01d31ffc26eb9032ac54a5a1c5949f3ae172d63
Jin An <jinsungan@gmail.com>
2021-01-11 15:38:32 -0500

    Update helloWorld.java

--- week1/helloWorld.java
+++ week1/helloWorld.java
@@ -1,3 +1,8 @@
-// Jin An
+class Main {
+  public static void main(String[] args) {
+    //line 2 is equivalent of int main()
 
-System.out.println("Hello World");
+    System.out.println("Hello world!");
+    //line 5 is equivalent to cout << "Hello World!";
+  }
+}
